62009d2ebbec9cb4ba45263c758bcbd393c9639f
huangnan1 <306999508@qq.com>
2016-05-05 12:46:39 +0800

    create project

---
+++ hnBlog_Server/routes/index.js
@@ -0,0 +1,23 @@
+﻿var express=require('express');
+var router = express.Router();
+
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+/**
+ *  登陆
+ */
+router.post('/login', function (req, res) {
+    if (req.body.ume == 'huangnan' && req.body.pwd == '831023') {
+        res.send(result(0,'成功'));
+    }
+});
+
+function result(code,message){
+    return JSON.stringify({'code':code,'message':message});
+}
+module.exports = router;
---
+++ hnBlog_Web/app/scripts/app.js
@@ -0,0 +1,35 @@
+﻿var apiUrl = "http://localhost:3000/";
+angular.module('hnblog', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('index', {
+      url: '/',
+      title: 'hn博客',
+      templateUrl: 'views/blog-list.html',
+      controller: 'blogIndex'
+    }).state('login', {
+      url: '/login',
+      title: '登陆',
+      templateUrl: 'views/login.html',
+      controller: 'blogLogin'
+    });
+  }]).controller('blogLogin', ['$scope', '$http', function ($scope, $http) {
+    $scope.loginModel = {
+      userName: '',
+      passWord: '',
+      login: function () {
+        $http.post(apiUrl + 'login', {
+          'ume': this.userName,
+          'pwd': this.passWord
+        }, {'Content-Type': 'application/json'}).success(function (data) {
+            if(data.code===0)
+            {
+              console.log(data);
+             // window.location='/';
+            }
+        });
+      }
+    }
+  }]).controller('blogIndex', ['$scope', function ($scope) {
+    $scope.name = "黄楠";
+  }]);
